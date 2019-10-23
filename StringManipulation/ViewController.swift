02eb06978bab2a0ae9b08fc90ce51f9ad5217ec0
amjehadsarkar@route1.com <amjehadsarkar@route1.com>
2019-10-23 17:48:02 -0400

    Initial Commit

---
+++ StringManipulation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StringManipulation
+//
+//  Created by Jehad Sarkar on 2019-10-23.
+//  Copyright © 2019 itsjehad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
